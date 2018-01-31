6040e1dbf86c9320347003ed5bdeedf1c4b5af40
Fred Bradley <hello@fredbradley.co.uk>
2018-01-31 16:24:04 +0000

    Add entry-content to welcome section

--- template-parts/homepage-welcome.php
+++ template-parts/homepage-welcome.php
@@ -4,7 +4,7 @@
 		$cranleigh_settings['welcome'] = "<p class=\"alert alert-warning\">You can set this welcoming paragraph in your 'Cranleigh Options' settings panel under 'Appearance' in your <a href=\"/wp-admin\" target=\"_blank\">Admin Panel</a></p>";
 	}
 ?>
-<section class="welcome">
+<section class="welcome entry-content">
 		<div class="container">
 			<div class="row border-bottom">
 				<div class="col-sm-12">
